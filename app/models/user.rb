4e56a1e14747c03dabbbb6839b01db7c05175f54
maiyama18 <myam0118@gmail.com>
2019-09-29 16:23:43 +0900

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates :token, uniqueness: true
+  validates :password, length: { minimum: 8 }
 end
